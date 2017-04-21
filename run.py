12128f597c2747da1e494390f8940f153a8d8c85
Huyen Ha <huyenht@vp9.vn>
2017-04-21 22:54:52 +0700

    Add bittrex

--- run.py
+++ run.py
@@ -2,8 +2,11 @@ from __future__ import print_function
 import config
 import polo
 import btfx
+import btrx
 
-# p = polo.poloniex(config.POLO_APIKey, config.POLO_Secret)
+#p = polo.poloniex(config.POLO.APIKey, config.POLO.Secret)
 b = btfx.Client()
-# print(p.returnTicker())
+#print(p.returnTicker())
 print(b.ticker('btcusd'))
+br = btrx.Bittrex("api","secret")
+print(br.get_ticker('btc_usd'))
